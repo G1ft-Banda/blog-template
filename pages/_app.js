1b7c55a913dab53a991e75ec52b9089756173699
G1ft <nissibanda4231@gmail.com>
2022-08-23 10:51:11 +0200

    added viewport tag

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,10 @@ function MyApp({ Component, pageProps }) {
           crossOrigin="anonymous"
           async
         ></script>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
       </Head>
       <Header />
       <main className="container">
